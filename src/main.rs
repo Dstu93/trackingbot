4cd29e2bea04aec60077743a9545e9640275e76f
dstu <dstu93@noreply-github.com>
2018-04-16 20:40:15 +0200

    added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 
+extern crate postgres;
+extern crate serde;
+extern crate serde_json;
+extern crate teleborg;
+
+#[macro_use]
+extern crate serde_derive;
+
 
 fn main() {
     println!("Hello, world!");
