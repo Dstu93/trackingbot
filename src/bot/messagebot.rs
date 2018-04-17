5bfb2da9ffd561c0dc797bc9851ff69b39150d87
doenis <dstu93@users.noreply.github.com>
2018-04-17 19:01:03 +0200

    defined MessageBot trait

--- src/bot/messagebot.rs
+++ src/bot/messagebot.rs
@@ -1,4 +1,9 @@
 
-pub trait MessageBot{
+use bot::commands::traits::MessageCommand;
+use std::io::Error;
+use std::thread::JoinHandle;
 
+pub trait MessageBot{
+    fn apply(&mut self, cmds: Vec<Box<MessageCommand + Send + Sync>>);
+    fn start(&mut self) -> Result<JoinHandle<()>,Error>;
 }
