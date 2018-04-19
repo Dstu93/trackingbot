e81cdf579a611725c711e17f3284871f578f39d0
dstu <dstu93@noreply-github.com>
2018-04-19 11:58:17 +0200

    added logic in main

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,23 @@ mod bot;
 #[cfg(test)]
 mod tests;
 
-use bot::configuration::loader;
+use bot::configuration::loader as configloader;
+use bot::messagebot::{BotType,BotBuilder,MessageBot};
 use bot::configuration::config::ApplicationConfig;
+use bot::commands::factory::MessageCommandFactory;
 
 fn main() {
-    let config = loader::applicationconfig();
+   let config = configloader::applicationconfig().expect("could not load config");
 
     //mit BotBuilder den MessangerBot bauen
     //bot starten
     //warten auf thread join
+    let mut bot = BotBuilder::build(&config,BotType::Telegram);
+    let cmds = MessageCommandFactory::build(&config);
 
-    println!("Hello, world!");
+    bot.apply(cmds);
+
+    let mut handle = bot.start().expect("Bot konnte nicht gestartet werden");
+
+    handle.join(); //stopt den Main Thread bis der Thread des Bots terminiert und die Threads gejoint werden koennen.
 }
